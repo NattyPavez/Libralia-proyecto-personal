80b4dfc448fe8d1d33b40a273b022254dc48f971
Natalia <nattypavez@gmail.com>
2025-08-17 20:11:58 -0400

    chore: inicializa Libralia con perfil dev y Swagger OK

---
+++ src/test/java/com/nataliapavez/libralia/controller/UsuarioControllerTest.java
@@ -0,0 +1,24 @@
+//package com.nataliapavez.libralia.controller;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.web.servlet.MockMvc;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//@SpringBootTest
+//@AutoConfigureMockMvc
+//class UsuarioControllerTest {
+//
+//    @Autowired
+//    private MockMvc mvc;
+//
+//    @Test
+//    void obtenerLibrosLeidos_escenario1() {
+//        mvc.perform(get())
+//
+//
+//    }
+//}
